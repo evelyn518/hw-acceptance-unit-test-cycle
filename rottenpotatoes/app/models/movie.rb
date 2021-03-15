14b2f7860235be5a0cf9a54b30e5d91c30c3b78b
EC2 Default User <ec2-user@ip-172-31-13-37.us-east-2.compute.internal>
2021-03-15 00:27:17 +0000

    add hw3 feature file

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,10 +1,21 @@
 class Movie < ActiveRecord::Base
   def self.all_ratings
-    %w(G PG PG-13 NC-17 R)
+        a = Array.new
+        self.select("rating").uniq.each {|x| a.push(x.rating)}
+        a.sort.uniq
   end
   def self.similar_movies id
     director = Movie.find(id).director
     return nil if director.nil?
     Movie.where(director: director)
   end
+  def self.with_ratings(ratings_list)
+    if ratings_list == nil
+        self.all_ratings
+    end
+    self.where("rating": ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    # if ratings_list is nil, retrieve ALL movies
+    end
 end
